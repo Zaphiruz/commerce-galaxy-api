8a96d163f005531d24e01d43598f1d0086dfbb34
Jacob Thomas <jacobthomas428@gmail.com>
2024-05-11 19:58:24 -0700

    more setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: `mongodb://cluster0.pgvfszf.mongodb.net`,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+      database: process.env.DATABASE_NAME,
+      appname: 'Cluster0',
+      w: 'majority',
+      retryWrites: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      ssl: false,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+    TypeOrmModule.forFeature([]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { VersioningType } from '@nestjs/common';
+import * as compression from 'compression';
+import { config } from 'dotenv';
+import helmet from 'helmet';
+
 import { AppModule } from './app.module';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.use(compression());
+
+  app.use(helmet());
+
+  app.enableCors({
+    methods: ['GET', 'POST', 'PUT'],
+    origin: ['http://localhost:3000', 'http://localhost:8080']
+  });
+
+  await app.listen(8081);
 }
 bootstrap();
