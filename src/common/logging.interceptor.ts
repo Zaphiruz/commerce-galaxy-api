ba196cbe6c58cb0f50efb4b64d6ab750cee51ea0
Jacob Thomas <jacobthomas428@gmail.com>
2024-05-14 17:16:58 -0700

    log payloads

--- src/common/logging.interceptor.ts
+++ src/common/logging.interceptor.ts
@@ -1,5 +1,5 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
-import { IncomingMessage } from 'http';
+import { IncomingMessage, OutgoingMessage } from 'http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
@@ -8,15 +8,20 @@ export class LoggingInterceptor implements NestInterceptor {
     logger = new Logger(LoggingInterceptor.name)
 
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-        let req: IncomingMessage = context.getArgByIndex(0);
+        const ctx = context.switchToHttp();
 
-        this.logger.log(`Req: ${req.method} ${req.url} ${req.headers['x-forwarded-for'] || req.socket.remoteAddress}`);
+        const req = ctx.getRequest();
+
+        this.logger.log(`Req: ${req.method} ${req.url} ${req.headers['x-forwarded-for'] || req.socket.remoteAddress} ${JSON.stringify(req.body, null, 2)}`);
 
         const now = Date.now();
         return next
             .handle()
             .pipe(
-                tap(() => this.logger.log(`Res: ${req.method} ${req.url} ${req.headers['x-forwarded-for'] || req.socket.remoteAddress} ${Date.now() - now}`)),
+                tap((resBody) => {
+                    let res = ctx.getResponse();
+                    this.logger.log(`Res: ${req.method} ${req.url} ${req.headers['x-forwarded-for'] || req.socket.remoteAddress} ${res.statusCode} ${Date.now() - now} \n${JSON.stringify(resBody, null, 2)}`)
+                })
             );
     }
 }
