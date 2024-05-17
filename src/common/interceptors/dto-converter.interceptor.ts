95beb375a590461f45e889efc56f1785839a455e
Jacob Thomas <jacobthomas428@gmail.com>
2024-05-17 07:52:18 -0700

    rework casting. clean up file

--- src/common/interceptors/dto-converter.interceptor.ts
+++ src/common/interceptors/dto-converter.interceptor.ts
@@ -1,15 +1,15 @@
 import {
-  Injectable,
-  NestInterceptor,
-  ExecutionContext,
-  CallHandler,
-  StreamableFile,
-  PlainLiteralObject,
+	Injectable,
+	NestInterceptor,
+	ExecutionContext,
+	CallHandler,
+	StreamableFile,
+	PlainLiteralObject,
 } from '@nestjs/common';
 import {
-  ClassConstructor,
-  instanceToPlain,
-  plainToInstance,
+	ClassConstructor,
+	instanceToPlain,
+	plainToInstance,
 } from 'class-transformer';
 import { Document } from 'mongoose';
 import { Observable } from 'rxjs';
@@ -17,64 +17,43 @@ import { map } from 'rxjs/operators';
 
 @Injectable()
 export class DtoInterceptor<T> implements NestInterceptor {
-  dtoClass: ClassConstructor<T>;
+	dtoClass: ClassConstructor<T>;
 
-  constructor(dtoClass: ClassConstructor<T>) {
-    this.dtoClass = dtoClass;
-  }
+	constructor(dtoClass: ClassConstructor<T>) {
+		this.dtoClass = dtoClass;
+	}
 
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next
-      .handle()
-      .pipe(
-        map((res: PlainLiteralObject | Array<PlainLiteralObject>) =>
-          this.dedoc(res),
-        ),
-      )
-      .pipe(
-        map((res: PlainLiteralObject | Array<PlainLiteralObject>) =>
-          this.instantiate(res),
-        ),
-      )
-      .pipe(
-        map((res: PlainLiteralObject | Array<PlainLiteralObject>) =>
-          this.serialize(res),
-        ),
-      );
-  }
+	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		return next
+			.handle()
+			.pipe(
+				map((res: PlainLiteralObject | Array<PlainLiteralObject>) =>
+					this.handle(this.dtoClass, res),
+				),
+			);
+	}
 
-  dedoc(response) {
-    if (response instanceof StreamableFile) {
-      return response;
-    }
+	handle(dtoClass, response) {
+		if (response instanceof StreamableFile) {
+			return response;
+		}
 
-    if (Array.isArray(response)) {
-      return response.map((response) =>
-        response instanceof Document ? response.toObject() : response,
-      );
-    } else {
-      return response instanceof Document ? response.toObject() : response;
-    }
-  }
+		if (Array.isArray(response)) {
+			return response.map((response) => this.cast(dtoClass, response));
+		} else {
+			return this.cast(dtoClass, response);
+		}
+	}
 
-  instantiate(response) {
-    if (response instanceof StreamableFile) {
-      return response;
-    }
-
-    return plainToInstance(this.dtoClass, response, {
-      excludeExtraneousValues: true,
-    });
-  }
-
-  serialize(response) {
-    if (response instanceof StreamableFile) {
-      return response;
-    }
-
-    return instanceToPlain(response, {
-      excludeExtraneousValues: true,
-      strategy: 'excludeAll',
-    });
-  }
+	private cast(dtoClass, response) {
+		const nonDoc =
+			response instanceof Document ? response.toObject() : response;
+		const plainDoc = plainToInstance(dtoClass, nonDoc, {
+			excludeExtraneousValues: true,
+		});
+		return instanceToPlain(plainDoc, {
+			excludeExtraneousValues: true,
+			strategy: 'excludeAll',
+		});
+	}
 }
